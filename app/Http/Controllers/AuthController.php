4887afd2588e52b7b5b6608d9b77129bace516fc
João Espolador <jgespolador@gmail.com>
2025-11-15 20:28:11 -0300

    finalizado 010 - uso do

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,9 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo "loginSubmit Page";
+        echo $request->input('text_username');
+        echo '<br>';
+        echo $request->input('text_password');
     }
 
     public function logout()
