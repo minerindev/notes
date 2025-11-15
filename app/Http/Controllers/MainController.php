506cc16b8d4eaedb438479688a4a611842c99ec8
João Espolador <jgespolador@gmail.com>
2025-11-15 03:09:33 -0300

    finalizado 003

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
+    public function index($value)
     {
-        return view('main');
+        return view('main', ['value' => $value]);
     }
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,5 +7,7 @@
 </head>
 <body>
     <h1>Welcome!</h1>
+    <hr>
+    <h3>This value is: <?= $value ?></h3>
 </body>
 </html>
