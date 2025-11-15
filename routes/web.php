f6c56fc419bd80dad30657a1a9dbdee788e5cb21
João Espolador <jgespolador@gmail.com>
2025-11-15 03:31:57 -0300

    finaliando a limpeza 006

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,4 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello World!";
-});
 
-Route::get('/about', function () {
-    echo "About Us Page";
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
